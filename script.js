14d4ded101db3991d5ba0edec896a9903e8d9d97
utkarshcoder2025 <utkarshpateriya09@gmail.com>
2026-03-13 09:55:22 +0530

    update

--- script.js
+++ script.js
@@ -9,10 +9,15 @@ function switchCustomer(name){
 currentCustomer=name
 
 document.querySelectorAll(".tabs button").forEach(btn=>{
+
 btn.classList.remove("active")
+
 if(btn.innerText===name){
+
 btn.classList.add("active")
+
 }
+
 })
 
 showData()
@@ -67,8 +72,11 @@ e.preventDefault()
 let date=document.getElementById("date").value
 
 if(date===""){
+
 alert("तारीख चुनें")
+
 return
+
 }
 
 let record={
@@ -89,26 +97,3 @@ showData()
 })
 
 showData()
-
-
-/* INSTALL BUTTON */
-
-let deferredPrompt
-
-const installBtn=document.getElementById("installBtn")
-
-window.addEventListener("beforeinstallprompt",(e)=>{
-
-e.preventDefault()
-
-deferredPrompt=e
-
-installBtn.style.display="block"
-
-})
-
-installBtn.addEventListener("click",()=>{
-
-deferredPrompt.prompt()
-
-})
